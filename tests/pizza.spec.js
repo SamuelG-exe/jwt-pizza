4ba334bc98194977ad35a4dc88b343a8f7dd6374
Samuel Galbraith <samuelgalbraith.school@gmail.com>
2024-10-11 00:40:22 -0600

    Where are these messages going

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -366,7 +366,6 @@ import { test, expect } from 'playwright-test-coverage';
 
 
 
-  //login page
   await page.goto('http://localhost:5173/');
   await page.getByRole('link', { name: 'Login' }).click();
 
